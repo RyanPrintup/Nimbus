134e95226067ce96095fa3f5a5f20f23246ee329
wolfchase <rvallada@ucsc.edu>
2015-12-06 00:31:49 -0800

    Fix single value assignment

--- client.go
+++ client.go
@@ -1,99 +1,99 @@
 package nimbus
 
 import (
-    "net"
-    "fmt"
-    "strings"
+	"fmt"
+	"net"
+	"strings"
 )
 
 type Client struct {
-    Server   string
-    Port     string
-    Channels []string
+	Server   string
+	Port     string
+	Channels []string
 
-    conn      net.Conn
-    writer   *IRCWriter
-    reader   *IRCReader
+	conn   net.Conn
+	writer *IRCWriter
+	reader *IRCReader
 
-    listeners map[string][]Listener
+	listeners map[string][]Listener
 
-    Nick     string
-    RealName string
-    UserName string
-    Password string
+	Nick     string
+	RealName string
+	UserName string
+	Password string
 
-    // AutoRejoin  bool
-    // MaxRetries  int
+	// AutoRejoin  bool
+	// MaxRetries  int
 
-    // debug bool
+	// debug bool
 }
 
 func NewClient(server string, nick string, config Config) *Client {
-    client := &Client{
-        Server:   server,
-        Port:     config.Port,
-        Channels: config.Channels,
+	client := &Client{
+		Server:   server,
+		Port:     config.Port,
+		Channels: config.Channels,
 
-        listeners: make(map[string][]Listener),
+		listeners: make(map[string][]Listener),
 
-        Nick:   nick,
-        RealName: config.RealName,
-        UserName: config.UserName,
-        Password: config.Password,
-    }
-    return client
+		Nick:     nick,
+		RealName: config.RealName,
+		UserName: config.UserName,
+		Password: config.Password,
+	}
+	return client
 }
 
 func (c *Client) Connect(callback func(error)) error {
-    conn, err := net.Dial("tcp", c.Server + ":" + c.Port)
+	conn, err := net.Dial("tcp", c.Server+":"+c.Port)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    c.conn = conn
-    c.reader = NewIRCReader(conn)
-    c.writer = NewIRCWriter(conn)
+	c.conn = conn
+	c.reader = NewIRCReader(conn)
+	c.writer = NewIRCWriter(conn)
 
-    if c.Password != "" {
-        c.Send(PASS, c.Password)
-    }
+	if c.Password != "" {
+		c.Send(PASS, c.Password)
+	}
 
-    c.Send(USER, c.Nick, c.UserName, "0", "*", ":" + c.Nick)
-    c.Send(NICK, c.Nick)
+	c.Send(USER, c.Nick, c.UserName, "0", "*", ":"+c.Nick)
+	c.Send(NICK, c.Nick)
 
-    return nil
+	return nil
 }
 
 func (c *Client) Listen(ch chan<- error) error {
-    for {
-        message, err := c.reader.Read()
+	for {
+		message, err := c.reader.Read()
 
-        if err != nil {
-            fmt.Println(err)
-            return err
-        }
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
 
-        switch message.Command {
-            case PING:
-                c.Send(PONG, message.Trailing)
-            case RPL_WELCOME:
-                for _, channel := range c.Channels {
-                    c.Send(JOIN, channel)
-                }
+		switch message.Command {
+		case PING:
+			c.Send(PONG, message.Trailing)
+		case RPL_WELCOME:
+			for _, channel := range c.Channels {
+				c.Send(JOIN, channel)
+			}
 
-        }
+		}
 
-        fmt.Println(message.Raw)
-        c.Emit(message.Command, message)
-    }
+		fmt.Println(message.Raw)
+		c.Emit(message.Command, message)
+	}
 }
 
 func (c *Client) Send(raw ...string) {
-    message := ParseMessage(strings.Join(raw, " "))
-    c.writer.Write(message.Bytes())
+	message, _ := ParseMessage(strings.Join(raw, " "))
+	c.writer.Write(message.Bytes())
 }
 
 func (c *Client) Say(channel string, text string) {
-    c.Send(PRIVMSG, channel, text)
+	c.Send(PRIVMSG, channel, text)
 }
