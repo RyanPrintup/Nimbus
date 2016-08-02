fd3d5316080e1d1990ac704668bad2d6702c028f
wolfchase <rvallada@ucsc.edu>
2016-08-01 17:01:36 -0700

    Add logger

--- logger.go
+++ logger.go
@@ -1,1 +1,20 @@
 package nimbus
+
+import "log"
+
+// SetLogFlags will set flags for go's log. Set to no flags by default.
+func (c *Client) SetLogFlags(flags int) {
+	log.SetFlags(flags)
+}
+
+// Lprintln will log a message if level if greater than or equal to debug.
+func (c *Client) Lprintln(level int, message string) {
+	c.Lprintf(level, message+"\n")
+}
+
+// Lprintf is the same as Lprintln, except you can print with a format string and arguments.
+func (c *Client) Lprintf(level int, format string, v ...interface{}) {
+	if level <= c.debug {
+		log.Printf(format, v...)
+	}
+}
